ee5994afe69eab0e90cfca35a7dabf1b7c38407d
Olusols <81810711+Olusols@users.noreply.github.com>
2021-11-25 17:47:55 +0000

    That is why i change it

--- git/urls.py
+++ git/urls.py
@@ -5,4 +5,7 @@ from .views import home
 urlpatterns = [
     path('', home, name='index'),
     #This is another 
+    '''
+     i have been trying to make some changes sincee all this while.
+    '''
 ]
